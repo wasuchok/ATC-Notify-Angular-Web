68ee816bace3ed7022747f0c41ccd46cc3be18ca
wasuchok <116450019+wasuchok@users.noreply.github.com>
2026-03-05 14:59:15 +0700

    Update api.config.ts

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_BASE_URL =
   (import.meta as any)?.env?.NG_APP_API_URL ||
   (import.meta as any)?.env?.NG_APP_API_BASE ||
-  'http://localhost:3300/api/v1';
+  'https://api-anotix.aoyama-th.com/api/v1';
 
 export const REFRESH_TOKEN_ENDPOINT = '/auth/refresh';
